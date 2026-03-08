72f27dc51a08f8874301e547560f6a362afce3f3
Jose Francisco Gonzalez <jfgs1609@gmail.com>
2026-03-08 06:31:54 +0100

    Refactor: Update main window structure and page organization for improved navigation

--- main.py
+++ main.py
@@ -1,65 +1,37 @@
 #!/usr/bin/env python3
 """
-LIS Builder - Generador de instaladores .lis
+LIS Builder - Aplicación instaladora con GTK 4
 """
 
 import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, Gio
 
-from pages.home import HomePage
-from pages.files import FilesPage
-from pages.settings import SettingsPage
-from pages.installer import InstallerPage
-from pages.advanced import AdvancedPage
-from pages.build import BuildPage
+from pages.presentation import PresentationPage
+from pages.license import LicensePage
+from pages.administrator import AdministratorPage
+from pages.directories import DirectoriesPage
+from pages.options import OptionsPage
+from pages.finish import FinishPage
 
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         super().__init__(application=app)
-        self.set_title("LIS Builder")
-        self.set_default_size(900, 650)
-        
-        # Crear HeaderBar
-        header_bar = Gtk.HeaderBar()
-        self.set_titlebar(header_bar)
-        
-        # Botón Build (izquierda, antes del menú)
-        build_button = Gtk.Button()
-        build_icon = Gtk.Image()
-        build_icon.set_from_icon_name("system-run-symbolic")
-        build_button.set_child(build_icon)
-        build_button.set_tooltip_text("Generar archivo .lis")
-        build_button.connect("clicked", self.on_build_clicked)
-        header_bar.pack_start(build_button)
-        
-        # Menú (derecha)
-        menu_button = Gtk.MenuButton()
-        menu_icon = Gtk.Image()
-        menu_icon.set_from_icon_name("open-menu-symbolic")
-        menu_button.set_child(menu_icon)
-        
-        # Crear menú
-        menu_model = Gio.Menu.new()
-        menu_model.append("About", "app.about")
-        menu_model.append("Settings", "app.settings")
-        menu_model.append("Quit", "app.quit")
-        
-        menu_button.set_menu_model(menu_model)
-        header_bar.pack_end(menu_button)
+        self.set_title("LIS Builder - Instalador")
+        self.set_default_size(800, 600)
         
         # Crear un Stack para contener las páginas
         self.stack = Gtk.Stack()
         self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
         
         # Agregar las páginas al stack
-        self.stack.add_titled(HomePage(), "home", "Home")
-        self.stack.add_titled(FilesPage(), "files", "Files")
-        self.stack.add_titled(SettingsPage(), "settings", "Settings")
-        self.stack.add_titled(InstallerPage(), "installer", "Installer")
-        self.stack.add_titled(AdvancedPage(), "advanced", "Advanced")
-        self.stack.add_titled(BuildPage(), "build", "Build")
+        self.stack.add_titled(PresentationPage(), "presentation", "Presentation")
+        self.stack.add_titled(LicensePage(), "license", "License")
+        self.stack.add_titled(AdministratorPage(), "administrator", "Administrator")
+        self.stack.add_titled(DirectoriesPage(), "directories", "Directories")
+        self.stack.add_titled(OptionsPage(), "options", "Options")
+        self.stack.add_titled(FinishPage(), "finish", "Finish")
         
         # Crear un StackSidebar
         sidebar = Gtk.StackSidebar()
@@ -68,18 +40,13 @@ class MainWindow(Gtk.ApplicationWindow):
         # Crear una caja horizontal con el sidebar y el stack
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         hbox.append(sidebar)
+        hbox.append(self.stack)
         
         # Hacer que el stack ocupe más espacio
-        self.stack.set_hexpand(True)
-        hbox.append(self.stack)
+        hbox.set_child_packing(self.stack, True, True, 0, Gtk.PackType.START)
         
         # Establecer la caja como contenido de la ventana
         self.set_child(hbox)
-    
-    def on_build_clicked(self, widget):
-        """Generar el archivo .lis"""
-        print("Build clicked - Generando archivo .lis...")
-        # TODO: Implementar generación de .lis
 
 
 class MyApp(Gtk.Application):
